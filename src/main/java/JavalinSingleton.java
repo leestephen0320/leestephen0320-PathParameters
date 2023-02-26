e1f5a2b1792b8bd5d381bb32ec6626027e606c89
leestephen0320 <leestephen0320@gmail.com>
2023-02-26 04:51:47 +0000

    modified:   src/main/java/JavalinSingleton.java

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -19,7 +19,7 @@ public class JavalinSingleton {
         app.get("/firstname/{first}", ctx -> {
             
             //write code here
-            String first = ctx.pathParam("name");
+            String first = ctx.pathParam("first");
             ctx.result(first);
 
         });
